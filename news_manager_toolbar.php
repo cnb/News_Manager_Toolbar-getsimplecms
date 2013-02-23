74fcf53da5e146d7e1fe5b7708ced863c7747f62
cnb <carnav@gmail.com>
2013-02-23 18:35:11 +0100

    cleanup, version 0.1.1
    group globals, remove unneeded

--- news_manager_toolbar.php
+++ news_manager_toolbar.php
@@ -5,7 +5,7 @@ $thisfile = basename(__FILE__, ".php");
 register_plugin(
 	$thisfile,
 	'News Manager Toolbar',
-	'0.1',
+	'0.1.1',
 	'Carlos Navarro',
 	'http://www.cyberiada.org/cnb/',
 	'SA GS Admin Toolbar + News Manager integration'
@@ -14,12 +14,9 @@ register_plugin(
 add_action('sa_toolbar_disp','nm_sa_toolbar');
 
 function nm_sa_toolbar(){
-	global $SATB_MENU_ADMIN,$SATB_MENU_STATIC;
-	global $fullpath, $GSADMIN;
+	global $SATB_MENU_STATIC, $fullpath, $GSADMIN, $NMPAGEURL, $id;
 	$SATB_MENU_STATIC['news'] = array('title'=> i18n_r('news_manager/PLUGIN_NAME'),'url'=>$fullpath.$GSADMIN.'/load.php?id=news_manager');
 	$SATB_MENU_STATIC['add_news'] = array('title'=> '+ '.i18n_r('news_manager/NEW_POST'),'url'=>$fullpath.$GSADMIN.'/load.php?id=news_manager&edit');
-	
-	global $NMPAGEURL, $id;
 	if ($id == $NMPAGEURL) {
 		unset($SATB_MENU_STATIC['edit']);
 		if (isset($_GET['post'])) {
